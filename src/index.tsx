ac57ccc82e05d850bd8d3cb37cb9d45859ba2b30
MazaSebastian <mazasebastian@hotmail.com>
2026-02-26 13:01:46 -0300

    feat: setup PWA Service Worker with Workbox

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { GlobalStyles } from './styles/GlobalStyles';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
@@ -33,3 +34,8 @@ root.render(
     </QueryClientProvider>
   </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
